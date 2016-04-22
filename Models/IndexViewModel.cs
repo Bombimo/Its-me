1dac3c50984f596a6eeddfdd3080d3227075833e
Bombimo <simon.odermatt@hotmail.ch>
2016-04-22 16:40:45 +0200

    Added files via upload

---
+++ Models/IndexViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class IndexViewModel
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "EmailAddress is required.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Surname is required.")]
+        public string Nachname { get; set; }
+        public bool EmailSendenTrue { get; set; }
+        public string text { get; set; }
+        public Byte[] Bild { get; set; }
+    }
+}
---
+++ Models/NotenViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace MvcApplication1.Models
+{
+    public class NotenViewModel
+    {
+        [Required(ErrorMessage = "Grade is reqired.")]
+        [Range(1, 6, ErrorMessage = "Grade is not valid")]
+        public double Note { get; set; }
+        [Required(ErrorMessage = "Fach is required")]
+        public string Fach { get; set; }
+    }
+}
